35635d0bc76346d040d31a82cec87229eec042bb
yasir mohamud <ymohamed018@gmail.com>
2020-07-13 08:43:24 -0700

    adds files and unit tests

---
+++ code-challenges/LinkedListLibrary/LinkedListLibrary/LinkedListLibrary/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedListLibrary
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
