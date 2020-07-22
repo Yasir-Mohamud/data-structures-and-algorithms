a5808567638fe8e43ae006a59833a8cf64c10850
yasir mohamud <ymohamed018@gmail.com>
2020-07-21 18:25:10 -0700

    Adds all files

---
+++ code-challenges/FifiAnimalShelter/FIFOAnimalShelter/FIFOAnimalShelter/Class/AnimalShelter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Class
+{
+    class AnimalShelter
+    {
+        public string Dog { get; set; }
+        public string Cat { get; set; }
+
+        public AnimalShelter(string animal)
+        {
+            Dog = dog;
+            Cat = cat;
+        }
+
+        AnimalShelter Animal = new AnimalShelter();
+        
+    }
+}
---
+++ code-challenges/FifiAnimalShelter/FIFOAnimalShelter/FIFOAnimalShelter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using stack_and_queue;
+
+namespace FIFOAnimalShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+        }
+    }
+}
