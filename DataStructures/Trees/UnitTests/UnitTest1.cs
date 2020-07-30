20704894bfce2b1f09b862e885f128e8e38d0f41
yasir mohamud <ymohamed018@gmail.com>
2020-07-29 21:55:01 -0700

    adds all traversal and unit tests

--- DataStructures/Trees/UnitTests/UnitTest1.cs
+++ DataStructures/Trees/UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Trees;
 using Xunit;
 
@@ -25,6 +26,137 @@ namespace UnitTests
         }
 
         // TEST 3
+        [Fact]
+        public void CanAddLeftAndRightChildToTheRoot()
+        {
+            Tree<int> tree = new Tree<int>(1);
+            Node<int> leftChild = new Node<int>(2);
+            Node<int> rightChild = new Node<int>(3);
+            tree.Root.LeftChild = leftChild;
+            tree.Root.RightChild = rightChild;
+
+            Assert.Equal(3, tree.Root.RightChild.Value);
+            Assert.Equal(2, tree.Root.LeftChild.Value);
+        }
+
+        // Test 4
+        [Fact]
+        public void CanReturnAPreOrderTraversal()
+        {
+            Tree<int> tree = new Tree<int>();
+
+            Node<int> root = new Node<int>(1);
+            Node<int> b = new Node<int>(2);
+            Node<int> c = new Node<int>(3);
+            Node<int> d = new Node<int>(4);
+            Node<int> e = new Node<int>(5);
+            Node<int> f = new Node<int>(6);
+            Node<int> g = new Node<int>(7);
+
+            tree.Root = root;
+
+            root.LeftChild = b;
+            root.RightChild = c;
+
+            b.LeftChild = d;
+            b.RightChild = e;
+
+            c.LeftChild = f;
+            c.RightChild = g;
+
+
+            List<int> order = new List<int>()
+            {
+                 1,2,4,5,3,6,7
+            };
+
+          
+
+            List<int> preOrder = tree.PreOrder(tree.Root);
+
+
+            Assert.Equal(order, preOrder);
+        }
+
+        // Test 5
+        [Fact]
+        public void CanReturnAInOrderTraversal()
+        {
+            Tree<int> tree = new Tree<int>();
+
+            Node<int> root = new Node<int>(1);
+            Node<int> b = new Node<int>(2);
+            Node<int> c = new Node<int>(3);
+            Node<int> d = new Node<int>(4);
+            Node<int> e = new Node<int>(5);
+            Node<int> f = new Node<int>(6);
+            Node<int> g = new Node<int>(7);
+
+            tree.Root = root;
+
+            root.LeftChild = b;
+            root.RightChild = c;
+
+            b.LeftChild = d;
+            b.RightChild = e;
+
+            c.LeftChild = f;
+            c.RightChild = g;
+
+
+            List<int> order = new List<int>()
+            {
+               4,2,5,1,6,3,7
+            };
+
+
+
+            List<int> inOrder = tree.InOrder(tree.Root);
+
+
+            Assert.Equal(order, inOrder);
+        }
+
+
+
+        // Test 6
+        [Fact]
+        public void CanReturnAPostOrderTraversal()
+        {
+            Tree<int> tree = new Tree<int>();
+
+            Node<int> root = new Node<int>(1);
+            Node<int> b = new Node<int>(2);
+            Node<int> c = new Node<int>(3);
+            Node<int> d = new Node<int>(4);
+            Node<int> e = new Node<int>(5);
+            Node<int> f = new Node<int>(6);
+            Node<int> g = new Node<int>(7);
+
+            tree.Root = root;
+
+            root.LeftChild = b;
+            root.RightChild = c;
+
+            b.LeftChild = d;
+            b.RightChild = e;
+
+            c.LeftChild = f;
+            c.RightChild = g;
+
+
+            List<int> order = new List<int>()
+            {
+                 4,5,2,6,7,3,1
+            };
+
+
+
+            List<int> postOrder = tree.PostOrder(tree.Root);
+
+
+            Assert.Equal(order, postOrder);
+        }
 
     }
 }
