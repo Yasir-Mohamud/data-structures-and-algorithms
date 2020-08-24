1f284c823d98721bf3434bd4cde31affc3e3e578
yasir mohamud <ymohamed018@gmail.com>
2020-08-24 10:36:07 -0700

    commit

--- code-challenges/Left-joinLibrary/Left-joinLibrary/Left-join.cs
+++ code-challenges/Left-joinLibrary/Left-joinLibrary/Left-join.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using HashTableImplamentation;
+using System;
 
 namespace Left_joinLibrary
 {
     public class Class1
     {
+        public static void LeftJoin(HashTable<int> table1 , HashTable<int> table2)
+     { 
+         
+     }
     }
 }
