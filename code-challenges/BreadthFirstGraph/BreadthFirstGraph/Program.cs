db161618bb14b574455060cf65ac42f9935d706b
yasir mohamud <ymohamed018@gmail.com>
2020-08-25 21:25:07 -0700

    adds all files and updates readme

---
+++ code-challenges/BreadthFirstGraph/BreadthFirstGraph/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BreadthFirstGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<Nodes> BreadthFirst(Node<T> vertex)
+        {
+
+            List<Node> nodes<--new List();
+            Queue breadth<--new Queue();
+            breadth.Enqueue(vertex)
+
+
+        while (!breadth.IsEmty())
+            {
+                var front = breadth.Dequeue();
+                nodes.Add(front);
+
+                foreach (var child in front.Children)
+                {
+                    if (!child.visited)
+                    {
+                        child.Visited = true;
+                        breadth.Enqueue(child);
+                    }
+                }
+                return nodes;
+            }
+        }
+    }
