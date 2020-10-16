4a9c1da7ea46067083b8032feff8b52c80e8e6ad
yasir mohamud <ymohamed018@gmail.com>
2020-10-15 17:56:03 -0700

    inital commit

--- DataStructures/GraphImplementation/XUnitTestProject1/UnitTest1.cs
+++ DataStructures/GraphImplementation/XUnitTestProject1/UnitTest1.cs
@@ -106,11 +106,11 @@ namespace XUnitTestProject1
             Vertex<string> vertex5 = graph.AddVertex("Narnia");
             Vertex<string> vertex6 = graph.AddVertex("Naboo");
 
-            graph.AddDirectedEdge(vertex1, vertex2, 1);
-            graph.AddDirectedEdge(vertex2, vertex3, 2);
-            graph.AddDirectedEdge(vertex3, vertex4, 3);
-            graph.AddDirectedEdge(vertex4, vertex5, 4);
-            graph.AddDirectedEdge(vertex5, vertex6, 5);
+            graph.AddDirectedEdge(vertex1, vertex2, 50);
+            graph.AddDirectedEdge(vertex2, vertex3, 25);
+            graph.AddDirectedEdge(vertex3, vertex4, 25);
+            graph.AddDirectedEdge(vertex4, vertex5, 25);
+            graph.AddDirectedEdge(vertex5, vertex6, 25);
          
 
             List<Vertex<string>> Expected = new List<Vertex<string>>();
@@ -143,11 +143,11 @@ namespace XUnitTestProject1
             Vertex<string> vertex5 = graph.AddVertex("Narnia");
             Vertex<string> vertex6 = graph.AddVertex("Naboo");
 
-            graph.AddDirectedEdge(vertex1, vertex2, 1);
-            graph.AddDirectedEdge(vertex2, vertex3, 2);
-            graph.AddDirectedEdge(vertex3, vertex4, 3);
-            graph.AddDirectedEdge(vertex4, vertex5, 4);
-            graph.AddDirectedEdge(vertex5, vertex6, 5);
+            graph.AddDirectedEdge(vertex1, vertex2, 50);
+            graph.AddDirectedEdge(vertex2, vertex3, 25);
+            graph.AddDirectedEdge(vertex3, vertex4, 25);
+            graph.AddDirectedEdge(vertex4, vertex5, 25);
+            graph.AddDirectedEdge(vertex5, vertex6, 25);
 
 
             List<Vertex<string>> Expected = new List<Vertex<string>>();
@@ -167,42 +167,49 @@ namespace XUnitTestProject1
 
 
         [Fact]
-        public void CanTestEdgeCasesForBreadthFirstTraversal()
+        public void CanTestHappyPathForDepthFirstTraversal()
         {
             //Arrange
             Graph<string, int> graph = new Graph<string, int>();
 
 
-            Vertex<string> vertex1 = graph.AddVertex("Pandora");
-            Vertex<string> vertex2 = graph.AddVertex("Arendelle");
-            Vertex<string> vertex3 = graph.AddVertex("Metroville");
-            Vertex<string> vertex4 = graph.AddVertex("Monstroplolis");
-            Vertex<string> vertex5 = graph.AddVertex("Narnia");
-            Vertex<string> vertex6 = graph.AddVertex("Naboo");
-
-            graph.AddUndirectedEdge(vertex1, vertex2, 1);
-            graph.AddUndirectedEdge(vertex2, vertex3, 2);
-            graph.AddUndirectedEdge(vertex3, vertex4, 3);
-            graph.AddUndirectedEdge(vertex4, vertex5, 4);
-            graph.AddUndirectedEdge(vertex5, vertex6, 5);
+            Vertex<string> vertex1 = graph.AddVertex("A");
+            Vertex<string> vertex2 = graph.AddVertex("B");
+            Vertex<string> vertex3 = graph.AddVertex("C");
+            Vertex<string> vertex4 = graph.AddVertex("D");
+            Vertex<string> vertex5 = graph.AddVertex("E");
+            Vertex<string> vertex6 = graph.AddVertex("F");
+            Vertex<string> vertex7 = graph.AddVertex("G");
+            Vertex<string> vertex8 = graph.AddVertex("H");
 
 
-            List<Vertex<string>> Expected = new List<Vertex<string>>();
+            graph.AddDirectedEdge(vertex1, vertex2, 50);
+            graph.AddDirectedEdge(vertex1, vertex4, 25);
+            graph.AddDirectedEdge(vertex2, vertex4, 25);
+            graph.AddDirectedEdge(vertex2, vertex3, 25);
+            graph.AddDirectedEdge(vertex2, vertex7, 25);
+            graph.AddDirectedEdge(vertex4, vertex5, 25);
+            graph.AddDirectedEdge(vertex4, vertex6, 25);
+            graph.AddDirectedEdge(vertex4, vertex8, 25);
+            graph.AddDirectedEdge(vertex6, vertex8, 25);
+
+
+            HashSet<Vertex<string>> Expected = new HashSet<Vertex<string>>();
             Expected.Add(vertex1);
-            Expected.Add(vertex5);
             Expected.Add(vertex2);
             Expected.Add(vertex3);
-            Expected.Add(vertex6);
+            Expected.Add(vertex7);
             Expected.Add(vertex4);
+            Expected.Add(vertex5);
+            Expected.Add(vertex8);
+            Expected.Add(vertex6);
 
             List<Vertex<string>> BreadthFirst = graph.BreadthFirst(vertex1);
 
 
-            Assert.NotEqual(Expected, BreadthFirst);
+            Assert.Equal(Expected, BreadthFirst);
 
         }
-
-
 
     }
 }
